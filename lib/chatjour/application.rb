aa834541b2110a9d684931ad7ebbc310bd8b7650
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-15 21:08:57 -0400

    Remove that crap... starting fresh

--- lib/chatjour/application.rb
+++ lib/chatjour/application.rb
@@ -3,25 +3,7 @@ require 'dnssd'
 module Chatjour
   class Application
     def self.run(*args)
-      case args.shift
-        when "users"; users(*args)
-        else;         help
-      end
-    end
-    
-    def self.help
-      puts "Chatjour"
-      puts "Chat via Bonjour/DNSSD."
-      puts "\nUsage: chatjour <command> [args]"
-      puts
-      puts "  users"
-      puts "      Lists available users."
-      puts
-    end
-    
-    def self.users(*args)
-      puts "At some point, this will show you all the users."
-      puts
+      
     end
   end
 end
