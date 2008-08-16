29fdda92275ebe07c30deb37fa6d50e39d197226
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-15 21:23:25 -0400

    some traip and thread settings copied from gitjour. setup registering and and user listing.

--- lib/chatjour/application.rb
+++ lib/chatjour/application.rb
@@ -1,9 +1,58 @@
+require 'etc'
 require 'dnssd'
+
+Thread.abort_on_exception = true
  
 module Chatjour
   class Application
     def self.run(*args)
+      new(*args).run
+    end
+
+    def initialize(*args)
+      @port, @username = 1323, Etc.getlogin
+    end
+    
+    def run
+      register
+      users
+    end
+    
+    def register
+      tr = DNSSD::TextRecord.new
+      tr['description'] = @username
       
+      DNSSD.register(@username, "_chat._tcp", 'local', @port, tr.encode) do |rr|
+        puts "Registered #{@username} on port #{@port}. Starting Chatjour."
+      end
+    end
+
+    def discover(timeout = 5)
+      waiting_thread = Thread.current
+    
+      dns = DNSSD.browse "_chat._tcp" do |reply|
+        DNSSD.resolve reply.name, reply.type, reply.domain do |resolve_reply|
+          service = GitService.new(reply.name,
+                                   resolve_reply.target,
+                                   resolve_reply.port,
+                                   resolve_reply.text_record['description'].to_s)
+          begin
+            yield service
+          rescue Done
+            waiting_thread.run
+          end
+        end
+      end
+    
+      puts "Gathering for up to #{timeout} seconds..."
+      sleep timeout
+      dns.stop
+    end
+    
+    def users
+      list = []
+      discover { |obj| list << obj }
+      list
     end
   end
 end
