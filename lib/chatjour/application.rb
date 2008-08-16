d0e17456d068cb3b1f2efd802ff2683f987990c0
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-15 20:59:15 -0400

    Fix require in bin. Add in a run with dummy users command and a help command.

--- lib/chatjour/application.rb
+++ lib/chatjour/application.rb
@@ -2,6 +2,21 @@ require 'dnssd'
  
 module Chatjour
   class Application
-
+    def self.run(*args)
+      case args.shift
+        when "users"; users(*args)
+        else;         help
+      end
+    end
+    
+    def self.help
+      puts "Chatjour"
+      puts "Chat via Bonjour/DNSSD."
+      puts "\nUsage: chatjour <command> [args]"
+      puts
+      puts "  users"
+      puts "      Lists available users."
+      puts
+    end
   end
 end
