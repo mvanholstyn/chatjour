48787d5bd298e3fa328e1ad741b51f6de55342bf
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-16 17:02:04 -0400

    forms?

--- lib/chatjour/ncurses/view.rb
+++ lib/chatjour/ncurses/view.rb
@@ -2,49 +2,53 @@ module Chatjour
   module Ncurses
     class View
       def initialize(options = {})
-        @lines, @columns, @line, @buffer = [], [], 0, ""
+        @lines, @columns, @buffer = [], [], ""
         
         @window = ::Ncurses.initscr
-        @window.scrollok(true)
         @window.getmaxyx(@lines, @columns)
         @lines, @columns = @lines.first, @columns.first
-        ::Ncurses.cbreak
-        ::Ncurses.nodelay(@window, true)
-        prompt
-      end
-      
-      def prompt
-        ::Ncurses.mvprintw(@lines - 2, 0, " " * @columns)
-        ::Ncurses.mvaddstr(@lines - 2, 0, "> ")
-        ::Ncurses.refresh
+
+        @chat_line = 1
+        @chat = ::Ncurses.newwin(@lines - 3, @columns, 0, 0)
+        @chat.scrollok(true)
+        @chat.box(0, 0)
+        @chat.refresh
+        
+        # @input = ::Ncurses::Form::FIELD.new(3, @columns, @lines - 3, 0, 0 ,0)
+        # @form = ::Ncurses::Form.new_form([@input])
+        # @form.post_form
       end
       
       def write(output)
         output.each_line do |line|
-          if @line == @lines - 2
-            @line -= 1
-            @window.wscrl(1)
-          end
-          ::Ncurses.mvprintw(@line , 0, line)
-          @line += 1
+          @chat.move(@chat_line, 1)
+          @chat.printw(" #{output}\n")
+          @chat_line +=1 unless @chat_line == @lines - 4
+          @chat.box(0, 0)
+          @chat.refresh
         end
-        prompt
       end
-      
+
       def receive
-        # special characters just echo uglyness
-        case ch = @window.getch
-          when -1
-            # ignore
-          when 10 # Enter
-            prompt
-            input = @buffer
-            @buffer = ""
-            input
-          else
-            @buffer << ch.chr
-            nil
-        end
+        # ch = @window.getch
+        # return nil if ch == -1
+        # case ch
+        #   when 10
+        #     result = @buffer.dup
+        #     ::Ncurses::Form.form_driver(@form, ::Ncurses::Form::REQ_DEL_LINE)
+        #     @buffer.replace("")
+        #     return result
+        #   when 127
+        #     ::Ncurses::Form.form_driver(@form, ::Ncurses::Form::REQ_DEL_PREV)
+        #   when 260
+        #     ::Ncurses::Form.form_driver(@form, ::Ncurses::Form::REQ_LEFT_CHAR)
+        #   when 261
+        #     ::Ncurses::Form.form_driver(@form, ::Ncurses::Form::REQ_RIGHT_CHAR)
+        #   else
+        #     @buffer << ch.chr
+        #     # ::Ncurses::Form.form_driver(@form, ch)
+        # end
+        # nil
       end
       
       def display_users(users)
